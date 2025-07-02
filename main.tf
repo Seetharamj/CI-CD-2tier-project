e1fe622308b6bf17f0e0263750926ae95cebbf97
seetharam j <seetharamj704@gmail.com>
2025-07-02 19:06:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,21 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-# Lookup latest Amazon Linux 2 AMI in us-east-1
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-# EC2 Instance
-resource "aws_instance" "ec2_instance" {
-  ami           = data.aws_ami.amazon_linux.id
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type = "t2.micro"
-  key_name      = var.key_name
 
   tags = {
-    Name = "JenkinsEC2Instance"
+    Name = "MyEC2Instance"
   }
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.s3_bucket_name
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
 
   tags = {
-    Name = "JenkinsS3Bucket"
+    Name = "MyS3Bucket"
   }
 }
