70dbddbbe78ecdf2d89760802d9aa03dad475578
seetharam j <seetharamj704@gmail.com>
2025-07-02 18:38:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,5 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
 variable "key_name" {
-  description = "AWS EC2 Key Pair"
+  description = "EC2 key pair name"
   type        = string
 }
 
